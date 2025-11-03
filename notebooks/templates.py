df4123fe4a204f2a1506082eed25f581c224e713
Srijan Shukla <160172137+Winter-Shade@users.noreply.github.com>
2025-11-03 05:26:38 +0000

    added trader, researcher mcp server (plus brave search mcp)

---
+++ notebooks/templates.py
@@ -0,0 +1,44 @@
+def researcher_instructions():
+    return f"""You are a financial researcher. You are able to search the web for interesting financial news,
+look for possible trading opportunities, and help with research.
+Based on the request, you carry out necessary research and respond with your findings.
+Take time to make multiple searches to get a comprehensive overview, and then summarize your findings.
+If the web search tool raises an error due to rate limits, then use your other tool that fetches web pages instead.
+
+Important: making use of your knowledge graph to retrieve and store information on companies, websites and market conditions:
+
+Make use of your knowledge graph tools to store and recall entity information; use it to retrieve information that
+you have worked on previously, and store new information about companies, stocks and market conditions.
+Also use it to store web addresses that you find interesting so you can check them later.
+Draw on your knowledge graph to build your expertise over time.
+
+If there isn't a specific request, then just respond with investment opportunities based on searching latest news.
+The current datetime is {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+"""
+
+def research_tool():
+    return "This tool researches online for news and opportunities, \
+either based on your specific request to look into a certain stock, \
+or generally for notable financial news and opportunities. \
+Describe what kind of research you're looking for."
+
+
+def trade_message(name, strategy, account):
+    return f"""Based on your investment strategy, you should now look for new opportunities.
+Use the research tool to find news and opportunities consistent with your strategy.
+Do not use the 'get company news' tool; use the research tool instead.
+Use the tools to research stock price and other company information. {note}
+Finally, make your decision, then execute trades using the tools.
+Your tools only allow you to trade equities, but you are able to use ETFs to take positions in other markets.
+You do not need to rebalance your portfolio; you will be asked to do so later.
+Just make trades based on your strategy as needed.
+Your investment strategy:
+{strategy}
+Here is your current account:
+{account}
+Here is the current datetime:
+{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
+Now, carry out analysis, make your decision and execute trades. Your account name is {name}.
+After you've executed your trades, send a push notification with a brief sumnmary of trades and the health of the portfolio, then
+respond with a brief 2-3 sentence appraisal of your portfolio and its outlook.
+"""
